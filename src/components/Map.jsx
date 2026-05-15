c7b8cf9417e57e7b855de39e4816a6813c4ea36d
AhmedAhad1 <ahad74@gmail.com>
2026-05-15 15:12:08 +0600

    useGeolocation added

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -6,25 +6,42 @@ import {
   Popup,
   TileLayer,
   useMap,
-  useMapEvent,
   useMapEvents,
 } from "react-leaflet";
 import { useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 import { useEffect } from "react";
+import { useGeolocation } from "../custom-hooks/useGeolocation";
+import Button from "../components/Button";
 const Map = () => {
   const [searchParams, setSearchparams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const {
+    isLoading: isGeolocationLoading,
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeolocation();
+
   const { data: cities } = useCities();
+
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   useEffect(() => {
     if (lat && lng) setMapPosition([lat, lng]);
   }, [lat, lng]);
 
+  useEffect(() => {
+    if (geoLocationPosition) {
+      setMapPosition([geoLocationPosition.lat, geoLocationPosition.lng]);
+    }
+  }, [geoLocationPosition]);
+
   return (
     <div className={styles.mapContainer}>
+      <Button type="position" onClick={getPosition}>
+        {isGeolocationLoading ? "Loading..." : "Get Position"}
+      </Button>
       <MapContainer
         className={styles.map}
         center={mapPosition}
@@ -36,7 +53,10 @@ const Map = () => {
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         {cities.map((item) => (
-          <Marker position={[item.position.lat, item.position.lng]}>
+          <Marker
+            position={[item.position.lat, item.position.lng]}
+            key={item.id}
+          >
             <Popup>{item.notes}</Popup>
           </Marker>
         ))}
